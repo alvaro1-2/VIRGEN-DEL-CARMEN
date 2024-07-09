9164226b5534440697823bc9b748c8bc3ffe6d57
alvaro1-2 <98532731+alvaro1-2@users.noreply.github.com>
2024-07-08 22:42:27 -0500

    mejora 3
    fecha 08/07/24

---
+++ scripts/toggle.js
@@ -0,0 +1,9 @@
+// scripts/toggle.js
+function toggleDetails(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === 'none' || element.style.display === '') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
